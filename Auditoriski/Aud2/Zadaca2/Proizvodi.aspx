a76ffaca6e86993afd24dd82023e62cd99eb536a
Ilija Boshkov <mindcalamity@gmail.com>
2015-02-26 09:50:37 +0100

    Dodadena Aud 2. Zadaca 2.

---
+++ Auditoriski/Aud2/Zadaca2/Proizvodi.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Proizvodi.aspx.cs" Inherits="Zadaca" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style2 {
+            height: 19px;
+        }
+    </style>
+</head>
+<body>
+<form id="form1" runat="server">
+    <div>
+        <table align="center" style="height: 347px; width: 673px">
+            <tr>
+                <th colspan="2">
+                    <asp:Label ID="lblNaslov" runat="server" Text=""></asp:Label>
+                </th>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    Производи
+                </td>
+                <td class="auto-style2">
+                    Цени
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:ListBox Width="100%" Height="100%" ID="lbProizvodi" runat="server" style="margin-top: 0px" AutoPostBack="True" OnSelectedIndexChanged="lbProizvodi_SelectedIndexChanged"></asp:ListBox>
+                </td>
+                <td>
+                    <asp:ListBox Width="100%" Height="100%" ID="lbCeni" runat="server" AutoPostBack="True" OnSelectedIndexChanged="lbCeni_SelectedIndexChanged"></asp:ListBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:LinkButton ID="lbAuto" runat="server" OnClick="lbAuto_Click1">Каталог</asp:LinkButton>
+                </td>
+                <td>
+                    <asp:Label ID="lbBrPromeni" runat="server" Text=""></asp:Label>
+                </td>
+            </tr>
+        </table> 
+    </div>
+</form>
+</body>
+</html>
---
+++ Auditoriski/Aud2/Zadaca2/Proizvodi.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Zadaca : System.Web.UI.Page
+{
+    private string[] scifi = { "Научна Фантастика 1", "Научна Фантастика 2", "Научна Фантастика 3" };
+    private string[] tehnicka = { "Техничка Книга 1", "Техничка Книга 2", "Техничка Книга 3" };
+    private string[] avtomobili = { "Автомобил 1", "Автомобил 2", "Автомобил 3" };
+    private string[] ceniScifi = { "65", "64", "41" };
+    private string[] ceniTehnicki = { "23", "53", "32" };
+    private string[] ceniAvtomobili = { "1200", "1300", "2500" };
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack) return;
+        int id = 0;
+        int.TryParse(Request.QueryString["id"], out id);
+        lblNaslov.Text = Request.QueryString["ime"];
+        string[][] proizvodi = {tehnicka, scifi, avtomobili};
+        string[][] ceni = {ceniTehnicki, ceniScifi, ceniAvtomobili};
+        Response.Write(id);
+        lbProizvodi.DataSource = proizvodi[id];
+        lbProizvodi.DataBind();
+        lbCeni.DataSource = ceni[id];
+        lbCeni.DataBind();
+    }
+    protected void lbAuto_Click1(object sender, EventArgs e)
+    {
+        Response.Redirect("Katalog.aspx");
+    }
+    protected void lbProizvodi_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        lbCeni.SelectedIndex = lbProizvodi.SelectedIndex;
+        if (ViewState["brPromeni"] == null)
+            ViewState["brPromeni"] = 1;
+        else
+            ViewState["brPromeni"] = ((int) ViewState["brPromeni"]) + 1;
+        lbBrPromeni.Text = ViewState["brPromeni"].ToString();
+    }
+    protected void lbCeni_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        lbProizvodi.SelectedIndex = lbCeni.SelectedIndex;
+    }
+}
